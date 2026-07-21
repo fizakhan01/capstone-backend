b666104b1afd84a80d88c2d31a0b708207e0b49a
fizakhan01 <fizaabass404@gmail.com>
2026-07-21 20:08:56 +0530

    changes

--- frontend/src/services/API.js
+++ frontend/src/services/API.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const API = axios.create({
+const api = axios.create({
   baseURL: "http://localhost:5000/api",
   withCredentials: true,
 });
 
 
-export default API;
+export default api;
